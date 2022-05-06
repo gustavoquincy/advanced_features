d0bfe0acc3622ff9c7b844db0921237ff131a335
gustavoquincy <yuankaile@gmail.com>
2022-05-06 17:42:24 +0800

    Declaring and calling an `extern` function defined in another language

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
+extern "C" {
+    fn abs(input: i32) -> i32;
+}
+
 fn main() {
-    let mut v = vec![1, 2, 3, 4, 5, 6];
-
-    let r = &mut v[..];
-    
-    let (a, b) = r.split_at_mut(3);
-
-    // assert_eq!(a, &mut [1, 2, 3]);
-    assert_eq!(b, &mut [4, 5, 6]);
+    unsafe {
+        println!("Absolute value of -3 is {}", abs(-3));
+    }
 }
