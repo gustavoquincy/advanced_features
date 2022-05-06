b27f7f93ca3c18589e3c67884f82bd25a1cf2da4
gustavoquincy <yuankaile@gmail.com>
2022-05-06 17:45:30 +0800

    `extern` block for ABI' '

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ extern "C" {
     fn abs(input: i32) -> i32;
 }
 
+#[no_mangle]
+pub extern "C" fn call_from_c() {
+    println!("Just calld a Rust function from C!");
+}
+
 fn main() {
     unsafe {
         println!("Absolute value of -3 is {}", abs(-3));
